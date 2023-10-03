b75753a9e118831aabd403d55f03123436388ee9
Lehoczky Zoltán <zoltan.lehoczky@betssongroup.com>
2023-10-03 14:47:38 +0200

    fix lint error

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
 
 import { AppModule } from "./app/app.module"
 
-var a = 10
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err))
